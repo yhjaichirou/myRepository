a0fdb3a576506a985e3f89dafcc6b220621dcd4d
yhjaichirou <835057159@qq.com>
2021-03-12 17:39:49 +0800

    大师傅士大夫

--- ProjectManager/src/main/java/com/fgw/project/Test.java
+++ ProjectManager/src/main/java/com/fgw/project/Test.java
@@ -1,5 +1,8 @@
 package com.fgw.project;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 
 import cn.hutool.core.date.DateUnit;
@@ -17,5 +20,51 @@ public class Test {
 		//相差一个月，31天
 		long betweenDay = DateUtil.between(date2,date1 , DateUnit.DAY);
 		System.out.println(s);
+		
+		
+		String cpcode = "AAAAAC";
+		String msg = "模板短信测试,0001";
+		String mobiles = "18607714703,18648807841";
+		String excode = "";
+		String templetid = "2";
+		String key = "f75b9467e1112239";
+		String md5source = cpcode+ msg+mobiles+excode+templetid+key;
+		md5source = "123456";
+		String md5str = "";
+		try {
+			md5str = makeMD5(new String(md5source.getBytes("UTF-8"), "UTF-8")).toLowerCase();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		System.out.println(md5str);
+		
+		
+	}
+	
+	
+	public static String makeMD5(String plainText) {
+		String re_md5 = new String();
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(plainText.getBytes("UTF-8"));
+			byte b[] = md.digest();
+			int i;
+			StringBuffer buf = new StringBuffer("");
+			for (int offset = 0; offset < b.length; offset++) {
+				i = b[offset];
+				if (i < 0)
+					i += 256;
+				if (i < 16)
+					buf.append("0");
+				buf.append(Integer.toHexString(i));
+			}
+			re_md5 = buf.toString().toLowerCase();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return re_md5;
 	}
 }
